13364490af8584d385fe533348da72c2b3e66152
Dawid Szłapa <dawidszlapaz@gmail.com>
2025-12-06 00:11:31 +0100

    Dtos returning Ids

--- backend/src/main/java/org/agh/backend/dto/DoctorDetailedDto.java
+++ backend/src/main/java/org/agh/backend/dto/DoctorDetailedDto.java
@@ -3,12 +3,14 @@ package org.agh.backend.dto;
 import org.agh.backend.model.Doctor;
 
 public class DoctorDetailedDto {
+    public Long id;
     public String name;
     public String surname;
     public String specialization;
     public String address;
 
     public DoctorDetailedDto(Doctor doctor) {
+        this.id = doctor.getId();
         this.name = doctor.getName();
         this.surname = doctor.getSurname();
         this.specialization = doctor.getSpecialization().getName();
--- backend/src/main/java/org/agh/backend/dto/DoctorDto.java
+++ backend/src/main/java/org/agh/backend/dto/DoctorDto.java
@@ -3,11 +3,13 @@ package org.agh.backend.dto;
 import org.agh.backend.model.Doctor;
 
 public class DoctorDto {
+    public Long id;
     public String name;
     public String surname;
     public String specialization;
 
     public DoctorDto(Doctor doctor) {
+        this.id = doctor.getId();
         this.name = doctor.getName();
         this.surname = doctor.getSurname();
         this.specialization = doctor.getSpecialization().getName();
