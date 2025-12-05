d5bca088126c7663e536d9f4b8b666dcbcb1b524
Dawid Szłapa <dawidszlapaz@gmail.com>
2025-12-06 00:02:43 +0100

    Added realistic data

--- backend/src/main/java/org/agh/backend/InitExampleDatabase.java
+++ backend/src/main/java/org/agh/backend/InitExampleDatabase.java
@@ -1,10 +1,7 @@
 package org.agh.backend;
 
 import jakarta.annotation.PostConstruct;
-import org.agh.backend.model.Doctor;
-import org.agh.backend.model.Specialization;
 import org.agh.backend.service.DoctorService;
-import org.agh.backend.service.SpecializationService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,27 +11,25 @@ public class InitExampleDatabase {
     private final boolean init;
 
     public InitExampleDatabase(DoctorService doctorService,
-                               SpecializationService specializationService,
                                @Value("${example.database:false}") boolean init
     ) {
         this.doctorService = doctorService;
         this.init = init;
     }
 
-    // TODO: Make realistic data
     @PostConstruct
     public void init() {
         if (!init) {
             return;
         }
 
-        doctorService.addDoctor("Name1", "Surname1", "Pesel1", "Cardiology", "Address1");
-        doctorService.addDoctor("Name2", "Surname2", "Pesel2", "Cardiology", "Address2");
-        doctorService.addDoctor("Name3", "Surname3", "Pesel3", "Cardiology", "Address3");
-        doctorService.addDoctor("Name4", "Surname4", "Pesel4", "Pediatrics", "Address4");
-        doctorService.addDoctor("Name5", "Surname5", "Pesel5", "Pediatrics", "Address5");
-        doctorService.addDoctor("Name6", "Surname6", "Pesel6", "Dermatology", "Address6");
-        doctorService.addDoctor("Name7", "Surname7", "Pesel7", "Specialization4", "Address7");
+        doctorService.addDoctor("Jan", "Kowalski", "89012345678", "Kardiologia", "ul. Słoneczna 12, Warszawa");
+        doctorService.addDoctor("Anna", "Nowak", "75098765432", "Kardiologia", "ul. Kwiatowa 5, Kraków");
+        doctorService.addDoctor("Piotr", "Wiśniewski", "62045678901", "Kardiologia", "ul. Leśna 9, Gdańsk");
+        doctorService.addDoctor("Katarzyna", "Wójcik", "88023456789", "Pediatria", "ul. Długa 23, Wrocław");
+        doctorService.addDoctor("Tomasz", "Kowalczyk", "99034567890", "Pediatria", "ul. Krótka 7, Poznań");
+        doctorService.addDoctor("Magdalena", "Kamińska", "67012349876", "Dermatologia", "ul. Lipowa 18, Łódź");
+        doctorService.addDoctor("Marcin", "Zieliński", "81056723490", "Neurologia", "ul. Parkowa 11, Lublin");
 
         System.out.println("Example database initialized.");
     }
